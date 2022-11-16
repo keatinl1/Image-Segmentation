9dbe55a443dbb7e9939cabc6bef531e4dad57320
Luke Keating <keatinl1@tcd.ie>
2022-11-16 00:08:33 +0000

    renamed some vars to make easier to follow

--- colony_segmentation.py
+++ colony_segmentation.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-
 class ColonyCounter:
 
     def __init__(self):
@@ -24,15 +23,22 @@ class ColonyCounter:
         self.main()
         
     def preprocessing_function(self, final, center_coordinates):
-        kernel = np.ones((2, 2), np.uint8)
-        img_dilation = cv2.dilate(final, kernel, iterations= 5)
-        erosion = cv2.erode(img_dilation, kernel, iterations= 3)
-        final = cv2.circle(erosion, center_coordinates, self.radius, self.color, self.thickness)
-        
-        grey_final = cv2.cvtColor(final, cv2.COLOR_RGB2GRAY)
-        grey_final = cv2.subtract(255, grey_final)
+        '''
+            need black spots on white for the counter to work
+            hence we invert the colours
 
-        return grey_final, final
+        '''
+
+        kernel = np.ones((2, 2), np.uint8)
+        dilated = cv2.dilate(final, kernel, iterations= 5)
+        eroded = cv2.erode(dilated, kernel, iterations= 3)
+        
+        rgb_colonies_alone = cv2.circle(eroded, center_coordinates, self.radius, self.color, self.thickness)
+        grey_colonies_alone = cv2.cvtColor(rgb_colonies_alone, cv2.COLOR_RGB2GRAY)
+
+        inverse_grey_colonies = cv2.subtract(255, grey_colonies_alone)
+
+        return inverse_grey_colonies, rgb_colonies_alone
 
     def counter_function(self, grey_final):
         params = cv2.SimpleBlobDetector_Params()
@@ -52,38 +58,37 @@ class ColonyCounter:
 
         for image in self.images:
 
-            # 1 - declarations
-            nemo_std = cv2.imread(image)
-            nemo = cv2.cvtColor(nemo_std, cv2.COLOR_BGR2HSV)
+            # 1 - imports and conversions
+            original_image = cv2.imread(image)
+            hsv_original = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
 
-            mask = cv2.inRange(nemo, self.light_orange, self.dark_orange)
-            result = cv2.bitwise_and(nemo, nemo, mask=mask)
-            final = cv2.cvtColor(result, cv2.COLOR_HSV2RGB)
+            mask = cv2.inRange(hsv_original, self.light_orange, self.dark_orange)
+            masked_hsv = cv2.bitwise_and(hsv_original, hsv_original, mask=mask)
+            masked_rgb = cv2.cvtColor(masked_hsv, cv2.COLOR_HSV2RGB)
 
-            height, width, _ = final.shape
+            height, width, _ = masked_rgb.shape
             center_coordinates = (int(width/2), int(height/2))
 
 
             # 2 - preprocessing
-            grey_final, final = self.preprocessing_function(final, center_coordinates)
+            inverse_grey_colonies, rgb_colonies_alone = self.preprocessing_function(masked_rgb, center_coordinates)
 
 
             # 3 - counting
-            number_of_blobs = self.counter_function(grey_final)
+            number_of_colonies = self.counter_function(inverse_grey_colonies)
 
 
             # 4 - plotting
-            display = 'Colonies counted: ' + str(number_of_blobs)
-
-            f, axarr = plt.subplots(1,2)
-            axarr[0].imshow(nemo_std)
-            axarr[0].set_title('Before segmentation')
-            axarr[1].imshow(final)
-            axarr[1].set_title('After segmentation')
-            f.set_size_inches(15, 7.5)
-
-            plt.text(300, 1000, display, style='italic', bbox={
-                'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
+            figure, sub_fig_arr = plt.subplots(1,2)
+            figure.set_size_inches(15, 7.5)
+            
+            sub_fig_arr[0].imshow(original_image)
+            sub_fig_arr[0].set_title('Before segmentation')
+            sub_fig_arr[1].imshow(rgb_colonies_alone)
+            sub_fig_arr[1].set_title('After segmentation')
+            
+            display = 'Colonies counted: ' + str(number_of_colonies)
+            plt.text(300, 1000, display, style='italic', bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10})
 
             plt.show()
 
